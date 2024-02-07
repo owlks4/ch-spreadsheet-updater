39bc3b1331b35c6a3756e4493020c84b8b61fe4d
Olivercomet <115742200+owlks4@users.noreply.github.com>
2024-02-07 11:15:35 +0000

    Update step-by-step functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import * as Papa from 'papaparse';
 
@@ -16,24 +15,39 @@ What does it need to do?
 */
 
 let csvUploader = document.getElementById("csv-file-upload");
-let csvData = null;
+csvUploader.addEventListener("change", () => {setErrorMessage(""); setPositiveMessage("Now click the 'proceed' button.")});
+let loadedCsvData = [];
 let companiesHouseData = null;
 let instructionsDOMElement = document.getElementById("instructions");
 let curStepDOMElement = document.getElementById("cur-step");
 let errorMessage = document.getElementById("error-message");
+let positiveMessage = document.getElementById("positive-message");
 
 let proceedButton = document.getElementById("proceed-button");
 proceedButton.addEventListener("click",()=>{
     console.log("!")
     if (csvUploader.files == null || csvUploader.files.length == 0){
-        setErrorMessage("You can't proceed to the next step because you haven't uploaded a CSV file!");
+        setErrorMessage("You can't proceed to the next step because<br>you haven't uploaded a CSV file!");
     } else {
-        alert("Wow! We've got the CSV and will now perform the next step!")
+        loadCSV(csvUploader.files[0], false);
+        if (curInstruction < instructionsCycle.length - 1){
+            curInstruction++;
+        }
+        csvUploader.files = null;
+        csvUploader.value = null;
+        updateCurStepDOMElement();
+        setPositiveMessage("Thanks. The step has advanced and the URL has changed.<br>Now please do it again with the new URL above.")
     }
 });
 
+function setPositiveMessage(msg){
+    positiveMessage.innerHTML = msg;
+    errorMessage.innerHTML = "";
+}
+
 function setErrorMessage(msg){
     errorMessage.innerHTML = msg;
+    positiveMessage.innerHTML = "";
 }
 
 let requiredPostcodes = ["B10","B11","B25","B26","B27"];
@@ -49,49 +63,32 @@ let year = d.getFullYear();
 let month = d.getMonth();
 let day = d.getDate();
 
-let sixteenHundredToTwoThousandAndFive = "&incorporationFromDay=1&incorporationFromMonth=1&incorporationFromYear=1600&incorporationToDay=1&incorporationToMonth=1&incorporationToYear=2005&sicCodes=&dissolvedFromDay=&dissolvedFromMonth=&dissolvedFromYear=&dissolvedToDay=&dissolvedToMonth=&dissolvedToYear="
-let twoThousandAndFiveToToday = "&incorporationFromDay=1&incorporationFromMonth=1&incorporationFromYear=1600&incorporationToDay="+day+"&incorporationToMonth="+month+"&incorporationToYear="+year+"&sicCodes=&dissolvedFromDay=&dissolvedFromMonth=&dissolvedFromYear=&dissolvedToDay=&dissolvedToMonth=&dissolvedToYear="
+let midpoint_year = 2022;
+let sixteenHundredToMidpoint = "&incorporationFromDay=1&incorporationFromMonth=1&incorporationFromYear=1600&incorporationToDay=1&incorporationToMonth=1&incorporationToYear="+midpoint_year+"&status=active&sicCodes=&dissolvedFromDay=&dissolvedFromMonth=&dissolvedFromYear=&dissolvedToDay=&dissolvedToMonth=&dissolvedToYear="
+let midpointToToday = "&incorporationFromDay=1&incorporationFromMonth=1&incorporationFromYear="+midpoint_year+"&incorporationToDay="+day+"&incorporationToMonth="+month+"&incorporationToYear="+year+"&status=active&sicCodes=&dissolvedFromDay=&dissolvedFromMonth=&dissolvedFromYear=&dissolvedToDay=&dissolvedToMonth=&dissolvedToYear="
 
 requiredPostcodes.forEach((postcode) => {
-    addToInstructions(searchURLPrefix + "\companyNameIncludes=&companyNameExcludes=&registeredOfficeAddress="+postcode+sixteenHundredToTwoThousandAndFive)
-    addToInstructions(searchURLPrefix + "\companyNameIncludes=&companyNameExcludes=&registeredOfficeAddress="+postcode+twoThousandAndFiveToToday)
+    addToInstructions("("+postcode + ", active, incorporated pre-"+midpoint_year+")", searchURLPrefix + "\companyNameIncludes=&companyNameExcludes=&registeredOfficeAddress="+postcode+sixteenHundredToMidpoint)
+    addToInstructions("("+postcode + ", active, incorporated "+midpoint_year+"-present)", searchURLPrefix + "\companyNameIncludes=&companyNameExcludes=&registeredOfficeAddress="+postcode+midpointToToday)
 });
 
-function addToInstructions(url){
-    instructionsCycle.push("Go to the URL below, click the big green 'Download results' button,<br>then come back here and upload the CSV you just obtained.<br><br><a href="+url+" target='_blank'><strong>Click here to go to the URL</strong></a>")
+function addToInstructions(smallDesc,url){
+    instructionsCycle.push("<span class='smallDesc'>"+smallDesc+"</span><br><br>Go to the URL below, click the big green 'Download results' button,<br>then come back here and upload the CSV you just obtained.<br><br><a href="+url+" target='_blank'><strong>Click here to go to the step "+(instructionsCycle.length+1)+" URL</strong></a>")
 }
 
 updateCurStepDOMElement();
 
 function updateCurStepDOMElement(){
     curStepDOMElement.innerHTML = "Step "+(curInstruction+1)+" of "+instructionsCycle.length;
+    instructionsDOMElement.innerHTML = instructionsCycle[curInstruction];
 }
 
-instructionsDOMElement.innerHTML = instructionsCycle[curInstruction];
-
-
-let curDate = new Date().getFullYear();
-
-csvUploader.addEventListener("change", () => {loadCSV(csvUploader.files[0], false)});
-
-
-
-function loadCSV(file, isCH){
+function loadCSV(file){
     // Parse the CSV content
     const results = Papa.parse(file,{
         header:true,
         complete: function (results) {
-            if (isCH){
-                companiesHouseData = results;
-                onCSVloadComplete();
-            }
-            else {
-                csvData = results;
-                csvUploader.style = "display:none;"
-                companiesHouseCsvUploader.style = ""
-                alert("Now please upload a CSV downloaded from the advanced search page on the Companies House website.\n\nRemember that advanced search results clip at 5000 entries, so if your CSV only has 5000 entries, it means some results may have been clipped.");
-                companiesHouseCsvUploader.addEventListener("change", () => {loadCSV(companiesHouseCsvUploader.files[0], true)});
-            }
+                loadedCsvData.push(results);
         }
     });
 }
@@ -105,7 +102,7 @@ function onCSVloadComplete(){
         console.log(row[Object.keys(row)[0]]);
         let companiesHouseEquivalent = getCompaniesHouseDataCompanyWithName(row[Object.keys(row)[0]]);
         if (companiesHouseEquivalent != null){
-             row["company_status_"+(curDate)] = companiesHouseEquivalent["company_status"];
+             row["company_status_"+(year)] = companiesHouseEquivalent["company_status"];
         }
         d[i] = row;
     }
@@ -117,7 +114,6 @@ function onCSVloadComplete(){
 
     alert("And then look at nominatim too, to get the latlong");  //https://nominatim.openstreetmap.org/search?q=135+pilkington+avenue,+birmingham&format=json&polygon=1&addressdetails=1
 }
-
 
 function getCompaniesHouseDataCompanyWithName(name){
     let d = companiesHouseData.data;
